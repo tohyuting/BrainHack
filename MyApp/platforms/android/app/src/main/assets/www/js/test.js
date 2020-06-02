8dbbb2cf67a10a1b822824e9958896dafa2c9e9f
tohyuting <tohyuting@gmail.com>
2020-06-02 19:13:39 +0800

    Filled up chevrons for each section

--- MyApp/platforms/android/app/src/main/assets/www/js/test.js
+++ MyApp/platforms/android/app/src/main/assets/www/js/test.js
@@ -15,20 +15,99 @@ window.fn.load = function(page) {
 document.addEventListener('init', function(event) {
   var page = event.target;
 
-  if (page.id === 'page1') {
-    page.querySelector('#chevronclick').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
+  if (page.id === 'LLLActivities') {
+    page.querySelector('#LLLFirst').onclick = function() {
+      document.querySelector('#LLLNavigator').pushPage('LLLActivity1.html', {data: {title: 'First Activity'}});
     };
-    page.querySelector('#chevronclick2').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('page3.html', {data: {title: 'Page 3'}});
+    page.querySelector('#LLLSecond').onclick = function() {
+      document.querySelector('#LLLNavigator').pushPage('LLLActivity2.html', {data: {title: 'Second Activity'}});
     };
-    page.querySelector('#pushing-button').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
+    page.querySelector('#LLLThird').onclick = function() {
+      document.querySelector('#LLLNavigator').pushPage('LLLActivity3.html', {data: {title: 'Third Activity'}});
     };
-  } else if (page.id === 'page2') {
+  } else if (page.id === 'LLLActivity1') {
     page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-    page.querySelector('#pushing2-button').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('page3.html', {data: {title: 'Page 3'}});
+  } else if (page.id === 'LLLActivity2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'LLLActivity3') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+});
+
+document.addEventListener('init', function(event) {
+  var page = event.target;
+
+  if (page.id === 'HWActivities') {
+    page.querySelector('#HWFirst').onclick = function() {
+      document.querySelector('#HWNavigator').pushPage('HWActivity1.html', {data: {title: 'Get Fit With Me'}});
     };
+    page.querySelector('#HWSecond').onclick = function() {
+      document.querySelector('#HWNavigator').pushPage('HWActivity2.html', {data: {title: 'HPB Workout'}});
+    };
+    page.querySelector('#HWThird').onclick = function() {
+      document.querySelector('#HWNavigator').pushPage('HWActivity3.html', {data: {title: 'Exercise Video'}});
+    };
+  } else if (page.id === 'HWActivity1') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'HWActivity2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'HWActivity3') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+});
+
+document.addEventListener('init', function(event) {
+  var page = event.target;
+
+  if (page.id === 'MWActivities') {
+    page.querySelector('#MWFirst').onclick = function() {
+      document.querySelector('#MWNavigator').pushPage('MWActivity1.html', {data: {title: 'Tips for Mental Wellbeing'}});
+    };
+    page.querySelector('#MWSecond').onclick = function() {
+      document.querySelector('#MWNavigator').pushPage('MWActivity2.html', {data: {title: 'Meditation Video'}});
+    };
+    page.querySelector('#MWThird').onclick = function() {
+      document.querySelector('#MWNavigator').pushPage('MWActivity3.html', {data: {title: 'Progressive Muscle Relaxation'}});
+    };
+  } else if (page.id === 'MWActivity1') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'MWActivity2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'MWActivity3') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+});
+
+document.addEventListener('init', function(event) {
+  var page = event.target;
+
+  if (page.id === 'LeiActivities') {
+    page.querySelector('#LeiFirst').onclick = function() {
+      document.querySelector('#LeiNavigator').pushPage('LeiActivity1.html', {data: {title: 'Come n’ Live @ Zaobao.sg'}});
+    };
+    page.querySelector('#LeiSecond').onclick = function() {
+      document.querySelector('#LeiNavigator').pushPage('LeiActivity2.html', {data: {title: 'PA e-courses'}});
+    };
+    page.querySelector('#LeiThird').onclick = function() {
+      document.querySelector('#LeiNavigator').pushPage('LeiActivity3.html', {data: {title: 'Another Leisure Video'}});
+    };
+  } else if (page.id === 'LeiActivity1') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'LeiActivity2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'LeiActivity3') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+});
+
+document.addEventListener('init', function(event) {
+  var page = event.target;
+
+  if (page.id === 'CookingActivities') {
+    page.querySelector('#CookingFirst').onclick = function() {
+      document.querySelector('#CookNavigator').pushPage('CookingActivity1.html', {data: {title: 'HPB Healthier Recipes'}});
+    };
+  } else if (page.id === 'CookingActivity1') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
   }
 });
