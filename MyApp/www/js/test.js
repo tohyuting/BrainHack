af898075d86cbe9e2c2086aa712cf3e25299a69e
tohyuting <tohyuting@gmail.com>
2020-06-02 15:46:54 +0800

    added chevron and navigator

--- MyApp/www/js/test.js
+++ MyApp/www/js/test.js
@@ -11,3 +11,24 @@ window.fn.load = function(page) {
   content.load(page)
     .then(menu.close.bind(menu));
 };
+
+document.addEventListener('init', function(event) {
+  var page = event.target;
+
+  if (page.id === 'page1') {
+    page.querySelector('#chevronclick').onclick = function() {
+      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
+    };
+    page.querySelector('#chevronclick2').onclick = function() {
+      document.querySelector('#myNavigator').pushPage('page3.html', {data: {title: 'Page 3'}});
+    };
+    page.querySelector('#pushing-button').onclick = function() {
+      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
+    };
+  } else if (page.id === 'page2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+    page.querySelector('#pushing2-button').onclick = function() {
+      document.querySelector('#myNavigator').pushPage('page3.html', {data: {title: 'Page 3'}});
+    };
+  }
+});
