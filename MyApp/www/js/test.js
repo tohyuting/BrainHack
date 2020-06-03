841935d1c5db7f94e3b85092c5eaaeadf5ab3358
tohyuting <tohyuting@gmail.com>
2020-06-03 13:12:34 +0800

    settled splitting menu

--- MyApp/www/js/test.js
+++ MyApp/www/js/test.js
@@ -140,13 +140,104 @@ document.addEventListener('init', function(event) {
   }
 });
 
-document.addEventListener("deviceready", onDeviceReady, false);
-document.addEventListener("backbutton", onBackKeyDown, false);
 
-function onDeviceReady() {
-  alert('deviceready')
-}
+document.addEventListener('init', function(event) {
+  var page = event.target;
 
-function onBackKeyDown() {
-  alert('backbutton press')
-}
+  if (page.id === 'home') {
+    page.querySelector('#english').onclick = function() {
+      document.querySelector('#myNav').pushPage('test2.html', {data: {title: 'HPB Healthier Recipes'}});
+    };
+  } else if (page.id === 'CovidNews') {
+    page.querySelector('#CovidFirst').onclick = function() {
+      document.querySelector('#myNav').pushPage('CovidNews1.html', {data: {title: 'New Cases of Covid-19'}});
+    };
+    page.querySelector('#CovidSecond').onclick = function() {
+      document.querySelector('#myNav').pushPage('CovidNews2.html', {data: {title: 'Resumption of Construction'}});
+    };
+    page.querySelector('#CovidThird').onclick = function() {
+      document.querySelector('#myNav').pushPage('CovidNews3.html', {data: {title: 'COVID-19 Laws in Force'}});
+    };
+    page.querySelector('#CovidFourth').onclick = function() {
+      document.querySelector('#myNav').pushPage('CovidNews4.html', {data: {title: 'Official MOH Twitter'}});
+    };
+  } else if (page.id === 'CovidNews1') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'CovidNews2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'CovidNews3') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'CovidNews4') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'LLLActivities') {
+    page.querySelector('#LLLFirst').onclick = function() {
+      document.querySelector('#myNav').pushPage('LLLActivity1.html', {data: {title: 'Using Youtube'}});
+    };
+    page.querySelector('#LLLSecond').onclick = function() {
+      document.querySelector('#myNav').pushPage('LLLActivity2.html', {data: {title: 'Using Zoom'}});
+    };
+    page.querySelector('#LLLThird').onclick = function() {
+      document.querySelector('#myNav').pushPage('LLLActivity3.html', {data: {title: 'Other Digital Skills'}});
+    };
+  } else if (page.id === 'LLLActivity1') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'LLLActivity2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'LLLActivity3') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'HWActivities') {
+    page.querySelector('#HWFirst').onclick = function() {
+      document.querySelector('#myNav').pushPage('HWActivity1.html', {data: {title: 'Get Fit With Me'}});
+    };
+    page.querySelector('#HWSecond').onclick = function() {
+      document.querySelector('#myNav').pushPage('HWActivity2.html', {data: {title: 'HPB Workout'}});
+    };
+    page.querySelector('#HWThird').onclick = function() {
+      document.querySelector('#myNav').pushPage('HWActivity3.html', {data: {title: 'More Exercise Video'}});
+    };
+  } else if (page.id === 'HWActivity1') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'HWActivity2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'HWActivity3') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'MWActivities') {
+    page.querySelector('#MWFirst').onclick = function() {
+      document.querySelector('#myNav').pushPage('MWActivity1.html', {data: {title: 'Tips for Mental Wellbeing'}});
+    };
+    page.querySelector('#MWSecond').onclick = function() {
+      document.querySelector('#myNav').pushPage('MWActivity2.html', {data: {title: 'Meditation Video'}});
+    };
+    page.querySelector('#MWThird').onclick = function() {
+      document.querySelector('#myNav').pushPage('MWActivity3.html', {data: {title: 'Muscle Relaxation'}});
+    };
+  } else if (page.id === 'MWActivity1') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'MWActivity2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'MWActivity3') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'LeiActivities') {
+    page.querySelector('#LeiFirst').onclick = function() {
+      document.querySelector('#myNav').pushPage('LeiActivity1.html', {data: {title: 'Entertainment'}});
+    };
+    page.querySelector('#LeiSecond').onclick = function() {
+      document.querySelector('#myNav').pushPage('LeiActivity2.html', {data: {title: 'PA e-courses'}});
+    };
+    page.querySelector('#LeiThird').onclick = function() {
+      document.querySelector('#myNav').pushPage('LeiActivity3.html', {data: {title: 'Arts'}});
+    };
+  } else if (page.id === 'LeiActivity1') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'LeiActivity2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'LeiActivity3') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  } else if (page.id === 'CookingActivities') {
+    page.querySelector('#CookingFirst').onclick = function() {
+      document.querySelector('#myNav').pushPage('CookingActivity1.html', {data: {title: 'HPB Healthier Recipes'}});
+    };
+  } else if (page.id === 'CookingActivity1') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+});
